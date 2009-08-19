a8a254dcaf29c3a2e9152d7d35fa9522c396ed94
Joshua Williams <jowillia@gmail.com>
2009-08-19 14:56:26 +0000

    del all
    git-svn-id: http://django-reader.googlecode.com/svn/trunk@5 1bffc2ee-8ccb-11de-b0b7-87b715de9b2a

--- src/django-reader/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from feedreader.models import Feed
-from django import forms
-
-class FeedForm(ModelForm):
-    class Meta:
-        model = Feed
--- src/django-reader/test_rss/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from feedparser.models import FeedForm
-from django import forms
-
-class FeedForm(ModelForm):
-    class Meta:
-        model = models.Feed
