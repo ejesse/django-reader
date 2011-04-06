11548eb6cc394c6cb94084de73267a66e63d01ea
Jesse Emery <jesse@onagertech.com>
2011-04-06 11:17:47 -0400

    cleaned up some formatting oddities

--- src/reader/tests.py
+++ src/reader/tests.py
@@ -15,54 +15,56 @@ import feedparser
 _PORT = 8097 # not really configurable, must match hardcoded port in tests
 
 class FeedParserTestRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-  headers_re = re.compile(r"^Header:\s+([^:]+):(.+)$", re.MULTILINE)
-  def send_head(self):
-    """Send custom headers defined in test case
+    headers_re = re.compile(r"^Header:\s+([^:]+):(.+)$", re.MULTILINE)
+    def send_head(self):
+        """Send custom headers defined in test case
+    
+        Example:
+        <!--
+        Header:   Content-type: application/atom+xml
+        Header:   X-Foo: bar
+        -->
+        """
+        path = self.translate_path(self.path)
+        headers = dict(self.headers_re.findall(open(path).read()))
+        f = open(path, 'rb')
+        headers.setdefault('Status', 200)
+        self.send_response(int(headers['Status']))
+        headers.setdefault('Content-type', self.guess_type(path))
+        self.send_header("Content-type", headers['Content-type'])
+        self.send_header("Content-Length", str(os.fstat(f.fileno())[6]))
+        for k, v in headers.items():
+            if k not in ('Status', 'Content-type'):
+                self.send_header(k, v)
+        self.end_headers()
+        return f
 
-    Example:
-    <!--
-    Header:   Content-type: application/atom+xml
-    Header:   X-Foo: bar
-    -->
-    """
-    path = self.translate_path(self.path)
-    headers = dict(self.headers_re.findall(open(path).read()))
-    f = open(path, 'rb')
-    headers.setdefault('Status', 200)
-    self.send_response(int(headers['Status']))
-    headers.setdefault('Content-type', self.guess_type(path))
-    self.send_header("Content-type", headers['Content-type'])
-    self.send_header("Content-Length", str(os.fstat(f.fileno())[6]))
-    for k, v in headers.items():
-      if k not in ('Status', 'Content-type'):
-        self.send_header(k, v)
-    self.end_headers()
-    return f
-
-  def log_request(self, *args):
-    pass
+    def log_request(self, *args):
+        pass
 
 class FeedParserTestServer(Thread):
-  """HTTP Server that runs in a thread and handles a predetermined number of requests"""
+    """HTTP Server that runs in a thread and handles a predetermined number of requests"""
   
-  def __init__(self, requests):
-    Thread.__init__(self)
-    self.requests = requests
-    self.ready = 0
+    def __init__(self, requests):
+        Thread.__init__(self)
+        self.requests = requests
+        self.ready = 0
     
-  def run(self):
-    self.httpd = BaseHTTPServer.HTTPServer(('', _PORT),
-					    FeedParserTestRequestHandler)
-    self.ready = 1
-    while self.requests:
-      self.httpd.handle_request()
-      self.requests -= 1
+    def run(self):
+        self.httpd = BaseHTTPServer.HTTPServer(('', _PORT),
+        				    FeedParserTestRequestHandler)
+        self.ready = 1
+        while self.requests:
+            self.httpd.handle_request()
+            self.requests -= 1
 
 class FeedparserTests(TestCase):
+    
     urls = 'reader.urls'
+    
     def setUp(self):
         self.client = Client()
-
+    
     def test_environment(self):
         """Just make sure everything is set up correctly."""
         self.assert_(True)
@@ -76,16 +78,16 @@ class FeedparserTests(TestCase):
         httpd = FeedParserTestServer(2)
         httpd.start()
         rssUrl = 'http://localhost:8097/src/reader/test_rss/test_1.rss'
-	tempRss = Feed(name="Craig's List Free", url=rssUrl)
-	newEntry = tempRss.fetch()
+        tempRss = Feed(name="Craig's List Free", url=rssUrl)
+        newEntry = tempRss.fetch()
         tempFeedList = FeedEntry.objects.all()
         self.assertEqual(len(tempFeedList), 100)
         self.assertEqual(len(newEntry), 100)
         tempFeedList = FeedEntry.objects.get(link='http://columbus.craigslist.org/zip/1326047327.html')
-	self.assertEqual(tempFeedList.title, 'at the curb: dresser and small table (Royal Dornoch Cir. Delaware 43015)')
-	newEntry = tempRss.fetch()
+        self.assertEqual(tempFeedList.title, 'at the curb: dresser and small table (Royal Dornoch Cir. Delaware 43015)')
+        newEntry = tempRss.fetch()
         tempFeedList = FeedEntry.objects.all()
-	self.assertEqual(len(tempFeedList), 100)
+        self.assertEqual(len(tempFeedList), 100)
         tempFeedList = FeedEntry.objects.get(link='http://columbus.craigslist.org/zip/1326047327.html')
         self.assertEqual(len(newEntry), 0)
 
